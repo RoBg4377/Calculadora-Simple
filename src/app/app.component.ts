82396519da7d5cdfdbaa8d5b0074e2cdd7b8054a
RoBg4377 <rhobg4377@gmail.com>
2022-09-23 12:52:46 -0300

    Practica de angular
    Ejercicio de aprendizaje de angula que muestra como ptabaja el data-bingdin los event- binding y los binding de doble vía.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Calculadora';
+  title = 'Calculadora Simple';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+  sum(): void {
+    this.result = this.num1 + this.num2;
+  }
+  rest(): void {
+    this.result = this.num1 - this.num2;
+  }
+  mult(): void {
+    this.result = this.num1 * this.num2;
+  }
+  div(): void {
+    this.result = this.num1 / this.num2;
+  }
 }
